bdd06ccedb45b362ff739658286c89b34be4a422
ranji <ranjithstar256@gmail.com>
2021-02-28 22:36:07 +0530

    Initial commit

---
+++ app/src/main/java/com/am/smartrefrigerator/WebBased.java
@@ -0,0 +1,81 @@
+package com.am.smartrefrigerator;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WebBased extends AppCompatActivity {
+    TextView textView,textView2;
+    String s1,s2,jsonstr;
+    ProgressDialog progressDialog;
+
+    JSONObject whl,obj7,obj8;
+    JSONArray jsonArray;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.web_based);
+        textView= findViewById(R.id.textView);
+        textView2= findViewById(R.id.textView2);
+
+    }
+
+    public void getcont(View view) {
+        GetContacts getContacts = new GetContacts();
+        getContacts.execute();
+    }
+
+    private class GetContacts extends AsyncTask {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog=new ProgressDialog(WebBased.this);
+            progressDialog.setMessage("Please wait");
+            progressDialog.show();
+        }
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+
+            Ragu ragu = new Ragu();
+          jsonstr = ragu.abc("https://api.androidhive.info/contacts/");
+
+            try {
+                whl= new JSONObject(jsonstr);
+
+                jsonArray= whl.getJSONArray("contacts");
+
+                obj7=jsonArray.getJSONObject(7);
+
+                s1=obj7.getString("name");
+                obj8=jsonArray.getJSONObject(8);
+
+                s2=obj8.getString("name");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        public void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            progressDialog.dismiss();
+
+            textView.setText(s1);
+            textView2.setText(s2);
+
+        }
+    }
+}
